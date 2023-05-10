a2b6a0277f3704e3f25b8e9979aa76ec9fb01c7e
tamerlan volhin <wiceum@gmail.com>
2023-05-10 14:05:53 +0500

    Метод calculate

--- app/Services/MealListService.php
+++ app/Services/MealListService.php
@@ -3,9 +3,10 @@
 namespace App\Services;
 
 use App\Exceptions\BusinessException;
-use App\Models\Ingredient;
 use App\Models\IngredientType;
+use Illuminate\Support\Arr;
 use Illuminate\Support\Collection;
+use Illuminate\Support\Facades\DB;
 
 final class MealListService
 {
@@ -27,7 +28,7 @@ final class MealListService
                 ->getIngredients()
                 ->checkIngredientTypes()
                 ->calculate();
-            return json_encode($this->result);
+            return json_encode($this->result, JSON_UNESCAPED_UNICODE);
         } catch (BusinessException $e) {
             return $e->getUserMessage();
         }
@@ -36,7 +37,7 @@ final class MealListService
     private function validateInput()
     {
         if (!preg_match('/^[a-z]*$/', $this->input)) {
-            throw new BusinessException('Error! Wrong input.');
+            throw new BusinessException('Wrong input! Only lowercase alphabetical letters allowed!');
         }
         return $this;
     }
@@ -44,15 +45,22 @@ final class MealListService
     private function getIngredients()
     {
         $this->ingredientTypes = IngredientType::all();
-        $this->ingredients = Ingredient::all();
+        $this->ingredients = DB::table('ingredient')
+            ->join('ingredient_type', 'ingredient.type_id', '=', 'ingredient_type.id')
+            ->select(['ingredient.id',
+                'ingredient.title',
+                'ingredient.price',
+                'ingredient_type.code',
+                DB::raw('ingredient_type.title as type_title')])
+            ->get();
         return $this;
     }
 
     private function checkIngredientTypes()
     {
-        $inputTypes = collect(str_split($this->input));
-        $intersected = $inputTypes->intersect($this->ingredientTypes);
-        if ($intersected->isNotEmpty()) {
+        $inputTypes = collect(str_split($this->input))->uniqueStrict();
+        $filtered = $inputTypes->diff($this->ingredientTypes->pluck('code'));
+        if ($filtered->isNotEmpty()) {
             throw new BusinessException('No such ingredient type!');
         }
         return $this;
@@ -60,6 +68,65 @@ final class MealListService
 
     private function calculate()
     {
+        $inputTypes = collect(str_split($this->input));
 
+        //создаем набор из разных комбинаций блюд, включая с одинаковыми ингредиентами
+        $combinations = collect(
+            $inputTypes->reduce(function (Collection $carry, $item) {
+                $ingredients = $this->ingredients->whereStrict('code', $item);
+                $carry = $carry->crossJoin($ingredients);
+                if ($carry->isEmpty()) {
+                    $carry = $ingredients;
+                }
+                return $carry;
+            }, collect())
+        )
+            ->map(function ($item) {
+                return collect($item)->flatten();
+            });
+
+        //добавляем в каждое блюдо поле с айдишниками входящих в него ингредиентов
+        $combinationsArray = $combinations->toArray();
+        foreach ($combinationsArray as $key => $combo) {
+            $ids = [];
+            foreach ($combo as $ingredient) {
+                $ids[] = $ingredient->id;
+            }
+            $ids = Arr::sort($ids);;
+            $combinationsArray[$key]['combo_ids'] = $ids;
+        }
+
+        //отбрасываем блюда где один ингредиент встречается несколько раз
+        $filtered = collect($combinationsArray)->map(function ($combo) {
+            $arr = collect($combo['combo_ids'])->uniqueStrict()->toArray();
+            $combo['combo_ids'] = $arr;
+            return $combo;
+        })
+            ->filter(function ($combo) use ($inputTypes) {
+                //объявление в начале метода
+                $res = count($combo['combo_ids']) === $inputTypes->count();
+                return $res;
+            })
+            ->map(function ($combo) {
+                $combo['combo_ids'] = implode('-', $combo['combo_ids']);
+                return $combo;
+            })
+            ->uniqueStrict('combo_ids')  //отбрасываем блюда с одинаковым набором ингредиентов
+            ->values();
+
+        $calculated = $filtered->map(function ($combo) {
+            $combo['price'] = collect($combo)->sum('price');
+            return $combo;
+        });
+
+        $prettified = $calculated->map(function ($combo) {
+            unset($combo['combo_ids']);
+            $price = $combo['price'];
+            unset($combo['price']);
+            return ['products' => $combo, 'price' => $price];
+        });
+
+        $this->result = $prettified;
     }
 }
+
